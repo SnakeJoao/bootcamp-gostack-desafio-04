323b5ac746f37b1c9f9e91412b3d55eed60de98c
Joao Guilherme <jguipanza@gmail.com>
2020-02-18 09:52:08 -0300

    feat: Versão final do projeto

---
+++ src/components/PostItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Comments from "./Comments";
+
+function PostHeader({ author, date }) {
+  return (
+    <div className="post-header">
+      <img className="avatar" src={author.avatar} />
+      <div className="details">
+        <span>{author.name}</span>
+        <span>{date}</span>
+      </div>
+    </div>
+  );
+}
+
+function PostItem({ author, date, content, comments }) {
+  return (
+    <div className="post">
+      <PostHeader author={author} date={date} />
+      <p className="post-content">{content}</p>
+      <Comments comments={comments} />
+    </div>
+  );
+}
+
+export default PostItem;
